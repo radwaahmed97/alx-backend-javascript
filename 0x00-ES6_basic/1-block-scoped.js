1423f2e52b61ba6a0322697e83fb09f437947e5b
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-07-22 14:24:13 -0400

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,14 +1,9 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock(TrueORFalse) {
   const task = false;
   const task2 = true;
 
-  if (trueOrFalse) {
-    // eslint-disable-next-line no-const-assign
-    const task = true;
-    const task2 = false;
-    if (task && task2) {
-      return [task, task2];
-    }
+  if (TrueORFalse) {
+    return [task, task2];
   }
   return [task, task2];
 }
