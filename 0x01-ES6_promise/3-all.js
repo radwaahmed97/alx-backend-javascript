daf623745a305ee66079f09e31affc3fc36187bb
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-07-24 14:47:43 -0400

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  if (uploadPhoto() && createUser()) {
-    Promise.all([uploadPhoto(), createUser()])
-      .then((values) => {
-        console.log(values[0].body, values[1].firstName, values[1].lastName);
-      })
-      .catch(() => console.log('Signup system offline'));
-  } else {
-    console.log('Error: Signup system offline');
-  }
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+  })
+    .catch((error) => {
+      console.error('Signup system offline', error);
+    });
 }
