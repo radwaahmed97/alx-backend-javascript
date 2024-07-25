18a7e928045e5636c5ed4489cdcff304b9ef175e
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-07-25 16:13:26 -0400

    9-hoisting.js

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -16,10 +16,10 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = 'holbertonClass';
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
@@ -39,5 +39,6 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const listOfStudents = [student1, student2, student3, student4, student5];
 
-export default [student1, student2, student3, student4, student5];
+export default listOfStudents;
