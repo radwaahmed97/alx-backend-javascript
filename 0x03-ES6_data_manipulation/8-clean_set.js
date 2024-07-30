428ddc7ddc7e1b830843a148d5b70cb5780e5e41
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-07-30 13:36:59 -0400

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
   const newset = [];
-  if (!startString) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  for (const value of set) {
+  for (const value of set.values()) {
     if (typeof value === 'string' && value.startsWith(startString)) {
       newset.push(value.substring(startString.length));
     }
