0c4191c26f57bb42f414f46aa672777b6d966a7c
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-17 13:33:36 -0400

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,16 @@
+const displayMessage = require('./0-console');
 /**
  * program named 1-stdin.js executed through command line
  * @param {string} input - input from command line
  * @returns {string} - output to command line
  */
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+displayMessage('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
-    process.stdout.write(`Your name is: ${input}\n`);
+    displayMessage(`Your name is: ${input}`);
   }
 });
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  displayMessage('This important software is now closing');
 });
