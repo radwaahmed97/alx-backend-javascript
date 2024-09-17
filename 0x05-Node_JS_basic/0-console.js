a5e5c2af6850e37205450bdce36551a84e9e4494
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-17 13:30:41 -0400

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Display a message to STDOUT
+ * @param {String} message the message to display
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
