0b3fac457e9dbd2cb870f252ded3673d231c36ac
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-18 16:40:30 -0400

    full_server/

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,14 +1,5 @@
-/**
- * App controller class
- */
-export default class AppController {
-  /**
-     * Handles req for `/` route
-     * @param {*} req   - request object
-     * @param {*} res  - response object
-     */
-  static getHomepage (req, res) {
-    res.statusCode = 200;
-    res.send('Hello Holberton School!');
+module.exports = class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
-}
+};
--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,58 +1,29 @@
-import readDatabase from '../utils';
-/**
- * controller class for students
- */
-export default class StudentsController {
-  /**
-     * sets the student database on response object
-     * @param {Object} request request object
-     * @param {Object} response response object
-     */
-  static getAllStudents (request, response) {
-    const body = ['This is the list of our students'];
-    readDatabase(process.argv[2] === undefined ? '' : process.argv[2])
-      .then((courseInfo) => {
-        for (const course in courseInfo) {
-          if (Array.isArray(courseInfo[course])) {
-            const students = courseInfo[course];
-            body.push(`Number of students in ${course}: ${students.length}. List: ${students.join(', ')}`);
+const readDatabase = require('../utils');
+
+module.exports = class StudentsController {
+  static getAllStudents(request, response) {
+    readDatabase(process.argv[2])
+      .then((data) => {
+        let printData = 'This is the list of our students';
+        for (const field in data) {
+          if (Object.hasOwnProperty.call(data, field)) {
+            const element = data[field];
+            printData += `\nNumber of students in ${field}: ${element.number}. ${element.students}`;
           }
         }
-        response.statusCode = 200;
-        response.send(body.join('\n'));
+        response.send(printData);
       })
-      .catch((error) => {
-        response.statusCode = 500;
-        response.send(error.message);
-      });
+      .catch((err) => { response.send(err.message); });
   }
 
-  /**
-     * search for students in request
-     * @param {Object} request request object
-     * @param {Object} response response object
-     */
-  static getAllStudentsByMajor (request, response) {
-    const major = request.params.major;
-    if (major !== 'CS' && major !== 'SWE') {
-      response.statusCode = 500;
-      response.send('Major parameter must be CS or SWE');
-    } else {
-      readDatabase(process.argv[2] === undefined ? '' : process.argv[2])
-        .then((courseInfo) => {
-          if (Array.isArray(courseInfo[major])) {
-            const students = courseInfo[major];
-            response.statusCode = 200;
-            response.send(`List: ${students.join(', ')}`);
-          } else {
-            response.statusCode = 500;
-            response.send('Cannot load the database');
-          }
-        })
-        .catch((error) => {
-          response.statusCode = 500;
-          response.send(error.message);
-        });
-    }
+  static getAllStudentsByMajor(request, response) {
+    if (!['SWE', 'CS'].includes(request.params.major)) response.status(500).send('Major parameter must be CS or SWE');
+    readDatabase(process.argv[2])
+      .then((data) => {
+        const printData = data[request.params.major].students;
+        if (printData) response.send(printData);
+        response.status(500).send('Major parameter must be CS or SWE');
+      })
+      .catch((err) => { response.send(err.message); });
   }
-}
+};
