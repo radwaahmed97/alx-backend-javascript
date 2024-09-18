81c6497d3571f3f8181865e598e8d210b10170c8
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-09-18 14:04:39 -0400

    3-read_file_async.js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,24 +2,24 @@ const fs = require('fs');
 
 /**
  * Counts the students in a CSV data file.
- * @param {String} Path The path to the CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
  */
-const countStudents = (Path) => new Promise((resolve, reject) => {
-    fs.readFile(Path, 'utf-8', (err, data) => {
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+    fs.readFile(dataPath, 'utf-8', (err, data) => {
         if (err) {
             reject(new Error('Cannot load the database'));
         }
         if (data) {
-            const fLines = data
+            const fileLines = data
                 .toString('utf-8')
                 .trim()
                 .split('\n');
             const studentGroups = {};
-            const dbaseFields = fLines[0].split(',');
-            const studentPropNames = dbaseFields
-                .slice(0, dbaseFields.length - 1);
+            const dbFieldNames = fileLines[0].split(',');
+            const studentPropNames = dbFieldNames
+                .slice(0, dbFieldNames.length - 1);
 
-            for (const line of fLines.slice(1)) {
+            for (const line of fileLines.slice(1)) {
                 const studentRecord = line.split(',');
                 const studentPropValues = studentRecord
                     .slice(0, studentRecord.length - 1);
@@ -32,10 +32,10 @@ const countStudents = (Path) => new Promise((resolve, reject) => {
                 studentGroups[field].push(Object.fromEntries(studentEntries));
             }
 
-            const numStudents = Object
+            const totalStudents = Object
                 .values(studentGroups)
                 .reduce((pre, cur) => (pre || []).length + cur.length);
-            console.log(`Number of students: ${numStudents}`);
+            console.log(`Number of students: ${totalStudents}`);
             for (const [field, group] of Object.entries(studentGroups)) {
                 const studentNames = group.map((student) => student.firstname).join(', ');
                 console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
