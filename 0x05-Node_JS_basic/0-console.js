d6e12b84b5201a8bdbe3f0c26bf3f20de7b029e7
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-11-26 14:19:19 -0500

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * @constructor Create a new console object
+ * @param {string} message - The message to display
+ * @return {void}
+ */
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
