60c6e6095060f8eab9c2e84afce6c5ba89a5404f
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-11-26 14:37:01 -0500

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', function() {
+    const INPUT = process.stdin.read();
+    if (INPUT) {
+        process.stdout.write(`Your name is: ${INPUT}\n`);
+    }
     });
-
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
     });
