b1b5b74ee496f0fec0a898cb8b43eefae06100fb
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-11-26 16:03:24 -0500

    2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 
 /**
  * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file. 
+ * @param {String} dataPath The path to the CSV data file.
+ * @return number of students
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
